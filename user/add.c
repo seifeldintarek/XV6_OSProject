7a555a549a7cea5cbbf433c4332e02ff20c0bdfe
Omarkh10 <khaledashta@gmail.com>
2025-12-12 20:24:38 +0200

    tail + cmds with help features

--- user/add.c
+++ user/add.c
@@ -5,46 +5,55 @@
 int
 main(int argc, char *argv[])
 {
-   if (argc != 3) {
+    // Help triggers
+    if (argc == 2 &&
+       (!strcmp(argv[1], "?") ||
+        !strcmp(argv[1], "-h") ||
+        !strcmp(argv[1], "--help"))) {
+
+        printf("Usage: add <num1> <num2>\n");
+        printf("Adds two integers (positive or negative).\n");
+        printf("\nExamples:\n");
+        printf("  add 3 4      # output: 7\n");
+        printf("  add -5 10    # output: 5\n");
+        printf("  add ?        # show this help\n");
+        exit(0);
+    }
+
+    // Must have 2 args
+    if (argc != 3) {
         printf("add uses three args\n");
         exit(1);
     }
-if(argv[1][0] >= 'a' || argv[1][0] >= 'z')
-  {
-      exit(3);
-  }
 
-  if(argv[1][0] >= 'A' || argv[1][0] >= 'Z')
-  {
-      exit(3);
-  }
+    // Input validation: check if first char is alphabetic
+    if ((argv[1][0] >= 'a' && argv[1][0] <= 'z') ||
+        (argv[1][0] >= 'A' && argv[1][0] <= 'Z')) {
+        exit(3);
+    }
 
-int n1,n2;
+    int n1, n2;
 
-  if(argv[1][0] == '-')
-  {
-    argv[1][0] = '0';
-    n1=atoi(argv[1]);
-    n1=-n1;
-  }
-  else n1=atoi(argv[1]);
+    // Handle negative sign
+    if (argv[1][0] == '-') {
+        argv[1][0] = '0';
+        n1 = atoi(argv[1]);
+        n1 = -n1;
+    } else {
+        n1 = atoi(argv[1]);
+    }
 
-  if (argv[2][0] == '-')
-  {
-    argv[2][0] = '0';
-    n2=atoi(argv[2]);
-    n2=-n2;
-  }
-  else
-  n2=atoi(argv[2]);
+    if (argv[2][0] == '-') {
+        argv[2][0] = '0';
+        n2 = atoi(argv[2]);
+        n2 = -n2;
+    } else {
+        n2 = atoi(argv[2]);
+    }
 
-  int sum=n1+n2;
+    int sum = n1 + n2;
 
-  printf("%d\n",sum);
+    printf("%d\n", sum);
 
-exit(0);
-
-
-
-
+    exit(0);
 }
--- user/sleep.c
+++ user/sleep.c
@@ -5,6 +5,21 @@
 int
 main(int argc, char *argv[])
 {
+  // Help triggers
+  if (argc == 2 &&
+     (!strcmp(argv[1], "--help") ||
+      !strcmp(argv[1], "-h") ||
+      !strcmp(argv[1], "?"))) {
+
+    printf("Usage: sleep <ticks>\n");
+    printf("Pause execution for a number of clock ticks.\n");
+    printf("\nExamples:\n");
+    printf("  sleep 10    # sleep for 10 ticks\n");
+    printf("  sleep 100   # sleep for 100 ticks\n");
+    printf("  sleep ?     # show this help message\n");
+    exit(0);
+  }
+
   if (argc != 2) {
     fprintf(2, "Usage: sleep <ticks>\n");
     exit(1);
@@ -16,6 +31,6 @@ main(int argc, char *argv[])
     exit(1);
   }
 
-  sleep(ticks); // call the system call
+  sleep(ticks);
   exit(0);
 }
